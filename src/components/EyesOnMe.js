32027378fd67179420269c05d6c2d2341b457d53
+ <+>
2023-05-05 11:11:25 -0400

    completed react event handling lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+
+function EyesOnMe(){
+
+    function blurEvent(){
+        console.log("Hey! Eyes on me!")
+    }
+
+    return(
+        <button onFocus={() => {
+            console.log("Good!")
+        }}
+        onBlur={blurEvent} >Eyes On Me</button>
+    )
+}
+
+export default EyesOnMe;
